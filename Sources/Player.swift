fc3a116414786a810f53fa94128a89ca37bb8e27
Carolina-S-Santos <aluno-111@10-41-50-5.0200.epoa.br.iosda.org>
2025-04-07 15:43:26 -0300

    Implement attack logic and verfication if player is alive

--- Sources/Player.swift
+++ Sources/Player.swift
@@ -1,8 +1,10 @@
 import Foundation
 
+
 class Player {
     var name: String?
-    var health: Int = 100
+    var health: Int = 2
+    
 
     init() {
     }
@@ -15,27 +17,37 @@ class Player {
         }
     }
 
-    func rollDice() -> Int {
-        return Int.random(in: 1...20)
+    func rollDice(_ sides : Int) -> Int {
+        return Int.random(in: 1...sides)
     }
 
-    func attack() -> Bool {
-        Thread.sleep(forTimeInterval: 2)
+    enum AttackResult {
+        case Success, Perfect, Fail
+    }
+    
+    func attack() -> AttackResult {
+//        Thread.sleep(forTimeInterval: 2)
         print("\n\(name ?? "Player")'s turn: Roll your d20 for attack!")
         _ = readLine()
 
-        let attackRoll = rollDice()
-        Thread.sleep(forTimeInterval: 1)
-        print("\n\(name ?? "Player") rolls a \(attackRoll) for attack!")
+        let attackRoll = rollDice(20)
+//        let attackRoll = 20
+//        Thread.sleep(forTimeInterval: 1)
+        print("\(name ?? "Player") rolls a \(attackRoll) for attack!")
 
-        if attackRoll > 10 {
-            Thread.sleep(forTimeInterval: 1)
+        if attackRoll > 10 && attackRoll != 20 {
+//            Thread.sleep(forTimeInterval: 1)
             print("\n\(name ?? "Player") attacks successfully!")
-            return true
+            return .Success
+            
+        } else if attackRoll == 20 {
+//            Thread.sleep(forTimeInterval: 1)
+            print("\n\(name ?? "Player") Used their ULT, you cannot run!")
+            return .Perfect
         } else {
-            Thread.sleep(forTimeInterval: 1)
+//            Thread.sleep(forTimeInterval: 1)
             print("\n\(name ?? "Player")'s attack failed.")
-            return false
+            return .Fail
         }
     }
 
@@ -44,52 +56,60 @@ class Player {
     }
     
     func defend() -> DefenseResult {
-        Thread.sleep(forTimeInterval: 2)
+//        Thread.sleep(forTimeInterval: 2)
         print("\n\(name ?? "Player")'s turn: Roll your d20 for defense!")
         _ = readLine()
 
-        let defendRoll = rollDice()
-        Thread.sleep(forTimeInterval: 1)
-        print("\n\(name ?? "Player") rolls a \(defendRoll) for defense!")
+        let defendRoll = rollDice(20)
+//        Thread.sleep(forTimeInterval: 1)
+        print("\(name ?? "Player") rolls a \(defendRoll) for defense!")
 
         if defendRoll > 12 && defendRoll != 20 {
-            Thread.sleep(forTimeInterval: 1)
+//            Thread.sleep(forTimeInterval: 1)
             print("\n\(name ?? "Player") defends successfully!")
             return .Success
         } else if defendRoll == 20 {
-            Thread.sleep(forTimeInterval: 1)
+//            Thread.sleep(forTimeInterval: 1)
             print("\n\(name ?? "Player") gets a counter attack")
             return .Counter
         } else {
-            Thread.sleep(forTimeInterval: 1)
+//            Thread.sleep(forTimeInterval: 1)
             print("\n\(name ?? "Player")'s defense failed.")
             return .Fail
         }
     }
 
     func damage() -> Int {
-        Thread.sleep(forTimeInterval: 2)
+//        Thread.sleep(forTimeInterval: 2)
         print("\n\(name ?? "Player") rolls for damage!")
         _ = readLine()
 
-        let damageRoll = rollDice()
-        Thread.sleep(forTimeInterval: 1)
-        print("\n\(name ?? "Player") deals \(damageRoll) damage!")
+        let damageRoll = rollDice(6)
+//        Thread.sleep(forTimeInterval: 1)
+        print("\(name ?? "Player") deals \(damageRoll) damage!\n")
         return damageRoll
     }
 
     func loseHealth(amount: Int) {
-        health -= amount
-        if health < 0 {
-            health = 0
+        self.health -= amount
+        if self.health < 0 {
+            self.health = 0
         }
     }
+    
+    func isPlayerAlive() -> Bool{
+        if self.health == 0 {
+            return false
+        }
+        return true
+    }
+    
     func status() {
         if let playerName = name {
-            Thread.sleep(forTimeInterval: 2)
+//            Thread.sleep(forTimeInterval: 2)
             print("\nPlayer: \(playerName), Health: \(health) HP")
         } else {
-            Thread.sleep(forTimeInterval: 2)
+//            Thread.sleep(forTimeInterval: 2)
             print("\nPlayer name is not set, Health: \(health) HP")
         }
     }
--- Sources/main.swift
+++ Sources/main.swift
@@ -17,48 +17,106 @@ player2.setName()
 
 @MainActor
 func playGame() {
-
-    if player1.attack() {
+    
+    let player1Attack = player1.attack()
+    switch player1Attack {
+    case .Success:
         let defense = player2.defend()
         if defense == .Success {
             return
         }else if defense == .Counter {
             player1.loseHealth(amount: player2.damage())
+//            print(!(player2.isPlayerAlive()))
+            if !(player1.isPlayerAlive()) {
+//                Thread.sleep(forTimeInterval: 2)
+                if let name = (player1.name){
+                    print("\(name) was eaten by a dragon, your journey ends here!")
+                    return
+                }
+            }
+            player1.status()
         }else {
             player2.loseHealth(amount: player1.damage())
+//            print(!(player1.isPlayerAlive()))
+            if !(player2.isPlayerAlive()) {
+//                Thread.sleep(forTimeInterval: 2)
+                if let name = (player2.name){
+                    print("\(name) was smashed by a troll, farewell young warrior!")
+                    return
+                }
+            }
+            player2.status()
         }
+    case .Perfect:
+        player2.loseHealth(amount: player1.damage())
+        if !(player2.isPlayerAlive()) {
+//            Thread.sleep(forTimeInterval: 2)
+            if let name = (player2.name){
+                print("\(name) tripped and fell into an abyss, bye bye!")
+                return
+            }        }
+        player2.status()
+    default: break
     }
 
-    if player2.attack() {
-        let defense = player1.defend()
-        if defense == .Success {
-            return
-        }else if defense == .Counter {
-            player2.loseHealth(amount: player1.damage())
-            
-        }else {
-            player1.loseHealth(amount: player2.damage())
+    let player2Attack = player2.attack()
+    switch player2Attack{
+    case .Success:
+            let defense = player1.defend()
+            if defense == .Success {
+                return
+            }else if defense == .Counter {
+                player2.loseHealth(amount: player1.damage())
+//                print(!(player2.isPlayerAlive()))
+                if !(player2.isPlayerAlive()) {
+//                    Thread.sleep(forTimeInterval: 2)
+                    if let name = (player2.name){
+                        print("\(name) was consumed by a hydra, its many heads fighting for the feast!")
+                        return
+                    }
+                    return
+                }
+                player2.status()
+            }else {
+                player1.loseHealth(amount: player2.damage())
+//                print(!(player1.isPlayerAlive()))
+                if !(player1.isPlayerAlive()) {
+//                    Thread.sleep(forTimeInterval: 2)
+                    if let name = (player1.name){
+                        print("\(name) (name) was turned to dust by a powerful curse, their soul lost to the void!")
+                        return
+                    }
+                }
+                player1.status()
+            }
+    case .Perfect:
+        player1.loseHealth(amount: player2.damage())
+        if !(player1.isPlayerAlive()) {
+//            Thread.sleep(forTimeInterval: 2)
+            if let name = (player1.name){
+                print("\(name) was dragged into a portal by a dark sorcery, their fate sealed in another dimension!")
+                return
+            }
         }
-    }
-
+        player1.status()
+    default: break
+        }
 }
 
 for round in 1...5 {
-    Thread.sleep(forTimeInterval: 2)
-    print("\n//----------------ROUND \(round)----------------//\n")
+//    Thread.sleep(forTimeInterval: 2)
+    print("\n//----------------ROUND \(round)----------------//")
     playGame()
     player1.status()
     player2.status()
-    if player1.health <= 0 {
-        Thread.sleep(forTimeInterval: 1)
-        print("\(player1.name ?? "Player 1") has been defeated!")
+    if player1.health == 0 || player2.health == 0{
+        print("\nGame Over!")
         break
-    } else if player2.health <= 0 {
-        Thread.sleep(forTimeInterval: 1)
-        print("\(player2.name ?? "Player 2") has been defeated!")
+    } else if round == 5 {
+        print("\nNo more rounds left.")
         break
-    }
-    if round == 5 {
-        print("Game Over! No more rounds left.")
     }
 }
+
+//Thread.sleep(forTimeInterval: 2)
+print("\n//----------------GAME OVER----------------//\n")
