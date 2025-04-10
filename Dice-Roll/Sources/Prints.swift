a57075990759bbb541590dcbd4c06aa4beaac210
Giulia Stefainski <giulia.chimini@gmail.com>
2025-04-10 10:11:52 -0300

    add song and prints class

---
+++ Dice-Roll/Sources/Prints.swift
@@ -0,0 +1,47 @@
+//
+//  Animations.swift
+//  Dice-Roll
+//
+//  Created by aluno-111 on 10/04/25.
+//
+
+class Animations {
+    
+    func printd20() {
+        
+    }
+    func printd6() {
+        
+    }
+    func printDragon() {
+        let dragonArt = """
+                                                                                        
+                                       ;++++xxX
+                                     +&XX&$&$x$X X
+                     xX            X&&&&$x+xx+$  ;
+                    $&$&X;$         +&&&Xx xXxx+             X$$$$$
+                  ;x$$&&$$$;        +$$$+x     ;X   X $$$&$Xxx+xxxxXXXX$X
+               $+X&XX$$$$$$X         &&XxX         $XX&&$XXX$$X$XXx++++++xXXXX
+              x++XX$XXx$XxX$        ;+&Xx++         $X$XxX$++++++++xXxxx+++xxXXX
+             x;;xxXXXxXXXXX$Xx         XXx;;;      XX$++xX++x&X++;++++x$XX++++xXX$
+           ;;;+;++XXXx$xXXX$$$x         x$X;;+     +$X++xx$x+++x$+;+;+;+XXx+++xxXXXX+
+          +;;++;+++$X$xxx+X$&$XXX       x$xx;+x   x$$+x+x++$x++++xx++++;;+xX++++xxxxxXX
+        $x+;+;+;+++x&Xxxxxxxx$$$$$X    x&Xxxxx+ X$$xxX++++++x;+++++&x+;+;;xxxx+;;xxx+  $
+         x++;;;;+XxxxXx&XxxxxxxxX$$XXXX&$XXxxx+$$X$x++x+++;;$x;;;;+x$x+;++xxxxx;;;x
+        ++;+;+xxXXXx+xXxxxxxx+xxxxX$&$XXXxXx+++xXx+x;;+;;;;;X;;;;;;+x+;;;+xxxxxxx+
+        xx;$XXxxXXXx++xXxx+++x+xxxxxX&$$$&xx;XxXxx+x+;;+++;++;;;;+++xXx;;;+xx+x+$+
+        $xxXXXXxx$xxx+++xx+++++++++x$&&XXX+X;;Xx:         +X:      :;Xx;+        X
+        $XXXXXXxxxxxxx+;++Xxx      :xX&&$XXx$$XX          +;          X+
+         &       ;x$x;      x$     $$$X$$$xX $XX                       x
+                   $x            $$$XX&X$$x   &$x
+                                 X&$XX$xxXXx  XXX
+                 XXXXXX&        x&X&XxXXXXxX   xXX+
+                       XX&     $&$X$$$XxX$X$      X
+                &&;     $$&$&&$$$XX$$$  xX  ;        x+
+               + $&$&$ ; X$$X$$xXX+&XXx+$&&++++++xXxx;
+                       XxxxxXX$xx+&$$&$$$$&&&$Xxxx ;
+                            +x Xxxxxxxxxxx
+        """
+        print(dragonArt)
+    }
+}
